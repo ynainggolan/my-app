244099cfa4120d3c8b8a069eeffc3f7e04f03ffa
ynainggolan <yhpnainggolan21@gmail.com>
2022-10-19 18:26:55 -0400

    adding clock component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Yoshi'
+  const element1 = <h1>Hello, {name}</h1>
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+  
+  const element2 = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
+
   return (
     <div className="App">
+      {element2}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Clock from './Clock';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Clock />
     <App />
   </React.StrictMode>
 );
